14b46ca3f2e007cc4afd129ad64296d7b95095c3
CAFUCH <tjfdk6201@gmail.com>
2024-03-05 18:34:57 +0900

    Defualt Setting

---
+++ GGM_Graduation_Project-2/Assets/01.Scripts/Core/Singleton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour       // 그냥 싱글턴
+{
+    static T instance;
+
+    [TextArea]
+    public string description;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;        // 없어요 없어 그냥 이거 나중에 지워도 되고 .... 
+
+                instance = GameObject.FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
