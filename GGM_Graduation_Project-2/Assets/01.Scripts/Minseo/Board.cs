d39139112a68c4407937fc2fb0838531df697464
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-03-21 19:19:42 +0900

    기본 슬라이딩 퍼즐

---
+++ GGM_Graduation_Project-2/Assets/01.Scripts/Minseo/Board.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tile;                        
+    [SerializeField]
+    private Transform boardTrm;                        
+
+    private List<Tile> tileList;                         
+
+    private int puzzleSize = 3;      
+    private float neighborTileDistance = 182;              
+
+    public Vector3 EmptyTilePosition { set; get; }        
+    public int Playtime { private set; get; } = 0;     
+    public int MoveCount { private set; get; } = 0; 
+
+    private IEnumerator Start()
+    {
+        tileList = new List<Tile>();
+
+        SpawnTiles();
+
+        UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(boardTrm.GetComponent<RectTransform>());
+
+        yield return new WaitForEndOfFrame();
+
+        tileList.ForEach(x => x.SetPosition());
+
+        StartCoroutine(Suffle());
+    }
+
+    private void SpawnTiles()
+    {
+        for (int y = 0; y < puzzleSize; ++y)
+        {
+            for (int x = 0; x < puzzleSize; ++x)
+            {
+                GameObject clone = Instantiate(this.tile, boardTrm);
+                Tile tile = clone.GetComponent<Tile>();
+
+                tile.Setup(this, puzzleSize * puzzleSize, y * puzzleSize + x + 1);
+
+                tileList.Add(tile);
+            }
+        }
+    }
+
+    private IEnumerator Suffle()
+    {
+        float current = 0;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            current += Time.deltaTime;
+            percent = current / 0.1f;
+
+            int index = UnityEngine.Random.Range(0, puzzleSize * puzzleSize);
+            tileList[index].transform.SetAsLastSibling();
+
+            yield return null;
+        }
+        EmptyTilePosition = tileList[tileList.Count - 1].GetComponent<RectTransform>().localPosition;
+    }
+
+    public void IsMoveTile(Tile tile)
+    {
+        if (Vector3.Distance(EmptyTilePosition, tile.GetComponent<RectTransform>().localPosition) == neighborTileDistance)
+        {
+            Vector3 goalPosition = EmptyTilePosition;
+
+            EmptyTilePosition = tile.GetComponent<RectTransform>().localPosition;
+
+            tile.OnMoveTo(goalPosition);
+
+            MoveCount++;
+        }
+    }
+}
---
+++ GGM_Graduation_Project-2/Assets/01.Scripts/Minseo/Tile.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Tile : MonoBehaviour, IPointerClickHandler
+{
+    private TextMeshProUGUI numText;
+    private Board board;
+    private Vector3 correctPosition;
+
+    public bool IsCorrected { private set; get; } = false;
+
+    private int numeric;
+    public int Numeric
+    {
+        set
+        {
+            numeric = value;
+            numText.text = numeric.ToString();
+        }
+        get => numeric;
+    }
+
+    public void Setup(Board board, int hideNumeric, int numeric)
+    {
+        this.board = board;
+        numText = GetComponentInChildren<TextMeshProUGUI>();
+
+        Numeric = numeric;
+        if (Numeric == hideNumeric)
+        {
+            GetComponent<UnityEngine.UI.Image>().enabled = false;
+            numText.enabled = false;
+        }
+    }
+
+    public void SetPosition()
+    {
+        correctPosition = GetComponent<RectTransform>().localPosition;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        board.IsMoveTile(this);
+    }
+
+    public void OnMoveTo(Vector3 end)
+    {
+        StartCoroutine(MoveTo(end));
+    }
+
+    private IEnumerator MoveTo(Vector3 end)
+    {
+        float current = 0;
+        float percent = 0;
+        Vector3 start = GetComponent<RectTransform>().localPosition;
+
+        while (percent < 1)
+        {
+            current += Time.deltaTime;
+            percent = current / 0.1f;
+
+            GetComponent<RectTransform>().localPosition = Vector3.Lerp(start, end, percent);
+
+            yield return null;
+        }
+    }
+
+}
