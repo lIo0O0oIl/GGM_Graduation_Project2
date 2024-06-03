8afb7490116fbc5e608bd117b4f422be68d973aa
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-03 14:05:57 +0900

    드래그 앤 드랍 제작
    - UI ToolKit에서 존재하는 VisaulElement를 게임 내에서 움직일 수 있게 해주었다.

--- GGM_Graduation_Project-2/Assets/ChatVisual/ChatStruct.cs
+++ GGM_Graduation_Project-2/Assets/ChatVisual/ChatStruct.cs
@@ -7,20 +7,20 @@ namespace ChatVisual
     public enum ESaveLocation
     {
         NotSave,
-        박현규,      // 조수
-        곽현석,      // 현석
-        강지현,        // 지현
-        황준원,         // 준원
-        이채민,
-        강대양
+        /*獄쏅벤?썸뉩?      // 鈺곌퀣??
+        ?⑥????      // ?袁⑷퐤
+        揶쏅벡???        // 筌왖??
+        ?????         // 餓Β??
+        ??곸퐟沃?
+        揶쏅베???*/
     }
 
     public enum EChatState
     {
-        Other = 0,      // 조수쪽이 말하는 것
-        Me = 1,      // 형사가 말하는 것
-        Ask = 2,        // 형사가 묻는 것임.
-        LoadNext        // 또 이어질 대화가 있다면. 다른 사람이면.      // Ask 랑 이건 사용안할 가능성이 있음 정리해주기 .
+        Other = 0,      // 鈺곌퀣?뷂쭫?뚯뵠 筌띾?釉??野?
+        Me = 1,      // ?類ㅺ텢揶쎛 筌띾?釉??野?
+        Ask = 2,        // ?類ㅺ텢揶쎛 ?얠궠??野껉퍔??
+        LoadNext        // ????곷선筌????遺? ??덈뼄筌? ??삘뀲 ???????      // Ask ????욧탷 ?????딅막 揶쎛?關苑????됱벉 ?類ｂ봺??곻폒疫?.
     }
 
     public enum EChatType
@@ -34,57 +34,57 @@ namespace ChatVisual
 
     public enum EFace
     {
-        Default,        // 무표정
-        Blush,          // 당황 (얼굴빨개짐.)
-        Difficult       // 비협조적인, 곤란한
+        Default,        // ?얜똾紐??
+        Blush,          // ?諭곸넺 (??⑤렗??몿而삼쭪?)
+        Difficult       // ??쑵?딂?怨쀬읅?? ?ⓦ끇???
     }
 
     public enum EChatEvent
     {
         Default,
-        Vibration,      // 텍스트 진동
-        Round,      // 파일 추가해주기      
-        Camera,     // 카메라 효과 넣어주기
+        Vibration,      // ??용뮞??筌욊쑬猷?
+        Round,      // ???뵬 ?곕떽???곻폒疫?     
+        Camera,     // 燁삳?李????ｋ궢 ?節뚮선雅뚯눊由?
     }
 
     [Serializable]
-    public class Chat      // 누가 어떤 말을 하는가
+    public class Chat      // ?袁? ??堉?筌띾Ŋ????롫뮉揶쎛
     {
-        public EChatState state;     // 말하는 것의 타입
-        public string text;        // 말 하는 것.
-        public EFace face;       // 말 할 때의 표정
+        public EChatState state;     // 筌띾?釉??野껉퍔??????
+        public string text;        // 筌???롫뮉 野?
+        public EFace face;       // 筌??????벥 ??뽰젟
         public List<EChatEvent> textEvent = new List<EChatEvent>();
     }
 
     [Serializable]
     public class AskAndReply
     {
-        public string ask;        // 물을 수 있는 선택지
-        public List<Chat> reply = new List<Chat>();     // 그에 대한 대답들
-        public bool is_UseThis;     // 사용했는지
+        public string ask;        // ?얠눘??????덈뮉 ?醫뤾문筌왖
+        public List<Chat> reply = new List<Chat>();     // 域밸챷肉?????????щ굶
+        public bool is_UseThis;     // ?????덈뮉筌왖
     }
 
     [Serializable]
     public class LockAskAndReply
     {
         public List<string> evidence = new List<string>();
-        public string ask;        // 물을 수 있는 선택지
-        public List<Chat> reply = new List<Chat>();     // 그에 대한 대답들
-        public bool is_UseThis;     // 사용했는지
+        public string ask;        // ?얠눘??????덈뮉 ?醫뤾문筌왖
+        public List<Chat> reply = new List<Chat>();     // 域밸챷肉?????????щ굶
+        public bool is_UseThis;     // ?????덈뮉筌왖
     }
 
     [Serializable]
     public class Chapter
     {
-        public string showName;     // 보여질 이름
-        public ESaveLocation saveLocation;     // 누군지
-        public List<Chat> chat = new List<Chat>();         // 챗팅
-        public List<AskAndReply> askAndReply = new List<AskAndReply>();           // 질문들
-        public List<LockAskAndReply> lockAskAndReply = new List<LockAskAndReply>();       // 잠긴 질문들
-        public List<string> round = new List<string>();           // 증거로 사용되어
+        public string showName;     // 癰귣똻肉э쭪???已?
+        public ESaveLocation saveLocation;     // ?袁㏓럵筌왖
+        public List<Chat> chat = new List<Chat>();         // 筌??샒
+        public List<AskAndReply> askAndReply = new List<AskAndReply>();           // 筌욌뜄揆??
+        public List<LockAskAndReply> lockAskAndReply = new List<LockAskAndReply>();       // ?醫됰┸ 筌욌뜄揆??
+        public List<string> round = new List<string>();           // 筌앹빓援끾에??????뤿선
 
-        public bool is_nextChapter;     // 다음챕터로 넘어가는지
-        public string nextChapterName;         // 다음 챕터로 넘어간다면 챕터의 인덱스 가 필요함
+        public bool is_nextChapter;     // ??쇱벉筌?벤苑ｆ에???뤿선揶쎛?遺?
+        public string nextChapterName;         // ??쇱벉 筌?벤苑ｆ에???뤿선揶쏄쑬?롳쭖?筌?벤苑???紐껊쑔??揶쎛 ?袁⑹뒄??
     }
 
     public class ChatStruct : MonoBehaviour { }
