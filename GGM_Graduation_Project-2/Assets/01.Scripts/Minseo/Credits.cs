32bc2c51b1bdac156ee72c73407875f5c5c5158f
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-08-27 19:53:47 +0900

    크레딧

---
+++ GGM_Graduation_Project-2/Assets/01.Scripts/Minseo/Credits.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private float scrollerSpeed;
+    [SerializeField] private float fadeSpeed;
+    [SerializeField] private float creditsLimit;
+
+    [SerializeField] private Image titleImg;
+    [SerializeField] private Image logoImg;
+
+    [SerializeField] private GameObject credits;
+
+    private RectTransform creditsRectTransform;
+    private bool isFade = false;
+    private bool isCreditsStopped = false;
+
+    private void Start()
+    {
+        creditsRectTransform = credits.GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (!isCreditsStopped)
+        {
+            if (isFade)
+            {
+                creditsRectTransform.anchoredPosition += Vector2.up * scrollerSpeed * Time.deltaTime;
+
+                if (creditsRectTransform.anchoredPosition.y >= creditsLimit)
+                {
+                    Debug.Log("Credits stopped");
+                    scrollerSpeed = 0;
+                    isCreditsStopped = true;
+                    StartCoroutine(FadeOutLogo());
+                }
+            }
+            else
+            {
+                StartCoroutine(FadeInTitle());
+            }
+        }
+    }
+
+    private IEnumerator FadeInTitle()
+    {
+        Color titleColor = titleImg.color;
+        while (titleImg.color.a < 1f)
+        {
+            titleColor.a += fadeSpeed * Time.deltaTime;
+            titleImg.color = titleColor;
+            yield return null;
+        }
+        isFade = true;
+    }
+
+    private IEnumerator FadeOutLogo()
+    {
+        Color logoColor = logoImg.color;
+        while (logoImg.color.a > 0f)
+        {
+            logoColor.a -= fadeSpeed * Time.deltaTime;
+            logoImg.color = logoColor;
+            yield return null;
+        }
+    }
+
+    public void GameBtn()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
